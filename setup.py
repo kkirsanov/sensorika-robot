2fd93dc943a1a3c3d68010013d9a1467c8221084
ASM <cpp.create@gmail.com>
2016-06-28 15:03:09 +0300

    Added setup.py and fixed typos

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#from setuptools import setup
+from distutils.core import  setup
+
+setup(name='zmq_robot',
+            description='Lightweight python middleware libriry of data exchange between mechatronics components. Sitable for mobile robotics and remote control.',
+            author='Kirill Kirsanov',
+            author_email='kkirsanov@gmail.com',
+            url='https://sensorika.info',
+            packages=['sensorika'],
+           )
