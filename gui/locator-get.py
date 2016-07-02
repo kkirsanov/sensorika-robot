928199c93f4f40725eb21afa865c69356c51dd42
Kirill Kirsanov <kkirsanov@gmail.com>
2016-07-02 10:42:03 +0300

    simple qt5 gui for database

---
+++ gui/locator-get.py
@@ -0,0 +1,102 @@
+from __future__ import unicode_literals
+
+import zmq
+
+ctx = zmq.Context()
+socket = ctx.socket(zmq.REQ)
+ip = '127.0.0.1'
+port = 15701
+socket.connect("tcp://" + ip + ":" + str(port))
+
+socket.send_json(dict(action='list'))
+d = socket.recv_json()
+print(d)
+
+socket.send_json(dict(action='get', count=10, name='wifi'))
+d = socket.recv_json()
+
+data = [val for times, val in d['data']]
+print(data)
+
+import sys
+import os
+import matplotlib
+
+# Make sure that we are using QT5
+matplotlib.use('Qt5Agg')
+from PyQt5 import QtCore, QtWidgets
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+progname = os.path.basename(sys.argv[0])
+progversion = "0.1"
+
+
+class MyMplCanvas(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+
+        self.compute_initial_figure()
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtWidgets.QSizePolicy.Expanding,
+                                   QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+
+class MyDynamicMplCanvas(MyMplCanvas):
+    def __init__(self, *args, **kwargs):
+        MyMplCanvas.__init__(self, *args, **kwargs)
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update_figure)
+        timer.start(1000)
+
+    def compute_initial_figure(self):
+        self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], 'r')
+
+    def update_figure(self):
+        global socket
+        socket.send_json(dict(action='get', count=90, name='wifi'))
+        d = socket.recv_json()
+
+        times = [val for times, val in d['data']]
+
+        self.axes.plot(times, 'r')
+        self.draw()
+
+
+class ApplicationWindow(QtWidgets.QWidget):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+
+        self.main_widget = QtWidgets.QWidget(self)
+
+        l = QtWidgets.QVBoxLayout(self.main_widget)
+
+        dc = MyDynamicMplCanvas(self.main_widget, width=7, height=4, dpi=100)
+
+        l.addWidget(dc)
+
+        self.main_widget.setFocus()
+
+
+qApp = QtWidgets.QApplication(sys.argv)
+
+aw = ApplicationWindow()
+aw.setWindowTitle("%s" % progname)
+aw.show()
+sys.exit(qApp.exec_())
+# qApp.exec_()
