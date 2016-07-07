6b2799d59d133c617ecae3f649710a1a4549cf5d
Kirill Kirsanov <kkirsanov@gmail.com>
2016-07-07 17:27:55 +0300

    roborino control form m5

--- control/robotino/robotino_control.py
+++ control/robotino/robotino_control.py
@@ -17,7 +17,7 @@ from examples.sensorika.inputs import get_gamepad
 class Joy(threading.Thread):
     def __init__(self, *a, **k):
         self.Estop = threading.Event()
-        self.joystat = dict(x=0, y=0, speed=0, grip=False, hobot=False, lx=0, ly=0, dx=0, dy=0, spin=0)
+        self.joystat = dict(x=0, y=0, invert=False, speed=0.2, grip=False, hobot=False, lx=0, ly=0, dx=0, dy=0, spin=0)
         super(Joy, self).__init__()
         self.thread = self.start()
 
@@ -40,10 +40,20 @@ class Joy(threading.Thread):
                         self.joystat['speed'] = event.state / 255.0
                     if event.code == 'ABS_RUDDER':
                         self.joystat['spin'] = event.state - 128
+                if abs(self.joystat['y']) < 0.07:
+                    self.joystat['y']
+                    self.joystat['y'] = 0
+                if abs(self.joystat['x']) < 0.07:
+                    self.joystat['x'] = 0
 
                 if event.ev_type == 'Sync':
                     continue
                 if event.ev_type == 'Key':
+                    print(event.code)
+                    if event.code == 'BTN_BASE5':
+                        if event.state == 1:
+                            self.joystat['invert'] = not self.joystat['invert']
+                        continue
                     if event.code == 'BTN_DEAD':
                         self.joystat['hobot'] = event.state == 1
                         continue
@@ -76,21 +86,28 @@ hobot = Connector(ns, l["data"]["port"])
 joy = Joy()
 
 move = [0, 0, 0]
-hob = {'en': False, 'grip': False, 'position': [0, 0, 0, 0, 0, 0]}
+hob = {'en': False, 'grip': False, 'position': [0, 0, 0, 0, 0, 0], 'invert': False}
 
-for x in range(1000):
-    move[0] = joy.joystat['speed'] * joy.joystat['dy'] * 500
-    move[1] = joy.joystat['speed'] * joy.joystat['dx'] * 500
-    move[2] = joy.joystat['speed'] * joy.joystat['lx'] * 250
+for x in range(1000000):
+    if joy.joystat['invert']:
+        print('inv')
+        move[0] =  joy.joystat['y'] * 500
+        move[1] = -joy.joystat['x'] * 500
+        move[2] = (-joy.joystat['spin'] / 2)
+    else:
+        print('norm')
+        move[0] = joy.joystat['speed'] * joy.joystat['dy'] * 500
+        move[1] = -joy.joystat['speed'] * joy.joystat['dx'] * 500
+        move[2] = joy.joystat['speed'] * joy.joystat['lx'] * 250
 
     hob['en'] = joy.joystat['hobot']
     hob['grip'] = joy.joystat['grip']
 
-    hob['position'][0] = joy.joystat['x']
-    hob['position'][1] = -joy.joystat['y']
+    hob['position'][0] = joy.joystat['x']  # * joy.joystat['speed']
+    hob['position'][1] = -joy.joystat['y']  # * joy.joystat['speed']
 
-    hob['position'][2] = joy.joystat['x']
-    hob['position'][3] = -joy.joystat['y']
+    hob['position'][2] = joy.joystat['x']  # * joy.joystat['speed']
+    hob['position'][3] = -joy.joystat['y']  # * joy.joystat['speed']
 
     if joy.joystat['spin'] > 0:
         hob['position'][4] = joy.joystat['spin']
@@ -99,8 +116,9 @@ for x in range(1000):
         hob['position'][4] = 0
         hob['position'][5] = -joy.joystat['spin']
 
-    print(hob['position'])
+    print(move, hob)
     time.sleep(0.1)
+    #if joy.joystat['speed']>=25:
     drive.set(move)
     hobot.set(hob)
 
